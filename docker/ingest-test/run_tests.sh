f908738ed28145e8fe9971eabbfb5d2bbc9c17a9
Alan King <alanking@renci.org>
2021-07-12 17:32:30 -0400

    test script seems to work

--- docker/ingest-test/run_tests.sh
+++ docker/ingest-test/run_tests.sh
@@ -5,19 +5,19 @@ set -e
 docker-compose --env-file icommands.env up &
 
 until [ $(docker container inspect -f '{{.State.Status}}' ingest-test_ingest-test_1) ]; do
-    echo "waiting for container to exist"
+    #echo "waiting for container to exist"
     sleep 1
 done
 
 while [ ! $(docker container inspect -f '{{.State.Status}}' ingest-test_ingest-test_1) == "running" ]; do
-    echo "waiting for container to run"
+    #echo "waiting for container to run"
     sleep 1
 done
 
-echo "test container is up"
+#echo "test container is up"
 
 while [ $(docker container inspect -f '{{.State.Status}}' ingest-test_ingest-test_1) == "running" ]; do
-    echo "waiting for tests to finish"
+    #echo "waiting for tests to finish"
     sleep 1
 done
 
--- docker/ingest-test/test/run_tests.sh
+++ docker/ingest-test/test/run_tests.sh
@@ -7,4 +7,6 @@ until nc -z icat.example.org 1247; do
     sleep 1
 done
 
+sleep 10
+
 python -m unittest ${TEST_CASE}
